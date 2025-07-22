05c736ec872b48a2ec194beca16821cdf69c134f
Sajal Talukder <sajaltalukder01@gmail.com>
2025-07-22 23:08:47 +0600

    new

---
+++ constant/constant.ts
@@ -0,0 +1,9 @@
+import { BarChart3, Building2, FileText, PenTool } from "lucide-react";
+
+export const navigation = [
+  { name: "Home", href: "/", icon: Building2 },
+  { name: "Share Story", href: "/share-story", icon: PenTool },
+  { name: "Reviews", href: "/reviews", icon: FileText },
+  { name: "Statistics", href: "/statistics", icon: BarChart3 },
+  { name: "Blog", href: "/blog", icon: FileText },
+];
